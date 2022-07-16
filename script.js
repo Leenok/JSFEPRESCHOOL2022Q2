10e175538caed87402751c5817d6dbf18e37b1b3
Leenok <34567431+Leenok@users.noreply.github.com>
2022-07-16 13:59:52 +0300

    init: update files

---
+++ script.js
@@ -0,0 +1,9 @@
+
+console.log("hi my love!");
+console.log(15+25);
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+});
